d8bddf1361d0b02793646c983dafcb21937340fc
chambesi <chambesi@mail.uc.edu>
2022-10-31 16:50:43 -0400

    corrected lab 8

--- Labs/lab8.py
+++ Labs/lab8.py
@@ -3,12 +3,16 @@ from bs4 import BeautifulSoup
 
 data = requests.get("https://viseartparis.com/collections/all-products").content
 soup = BeautifulSoup(data, 'html.parser')
-span = soup.find("a", {"class":"card-information__text h4"})
-title = span.text
+items = soup.find_all("div", {"class":"card-information__wrapper"})
+
+for item in items:
+
+#title = span.text
 #tags = soup.findAll("a", {"class":re.compile("(text)")})
 #for card in tags:
    # a = card.findAll("card-information__text h4",{"class":"a"})
     #print(a)
-span = soup.find("span", {"class":"price-item--regular"})
-price = span.text
-print("Item %s costs %s" % (title, price))
+    price = item.find("span", {"class":"money"}).text
+    name = item.find("a", {"class":"card-information__text"}).text
+    #print("Item %s costs %s" % (, price))
+    print(price, name)
